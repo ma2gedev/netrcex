ddda7874e4b7b734c4ac99586aa656114d069e19
Takayuki Matsubara <takayuki.1229@gmail.com>
2014-11-11 22:07:11 +0900

    add tests

--- test/netrc_test.exs
+++ test/netrc_test.exs
@@ -6,4 +6,17 @@ defmodule NetrcTest do
     assert is_map(netrc)
     assert Map.get(netrc, "m") == %{ "login" => "l", "password" => "p" }
   end
+
+  test "read sample netrc reversed" do
+    netrc = Netrc.read("test/data/sample_reverse.netrc")
+    assert is_map(netrc)
+    assert Map.get(netrc, "m") == %{ "login" => "l", "password" => "p" }
+  end
+
+  test "read sample netrc multi" do
+    netrc = Netrc.read("test/data/multi.netrc")
+    assert is_map(netrc)
+    assert Map.get(netrc, "m") == %{ "login" => "l", "password" => "p" }
+    assert Map.get(netrc, "m2") == %{ "login" => "l2", "password" => "p2" }
+  end
 end
