f758bf4bd67c87f493796025e818f13f70aa470b
Takayuki Matsubara <takayuki.1229@gmail.com>
2014-11-12 23:21:37 +0900

    no longer needed

--- test/netrc_test.exs
+++ test/netrc_test.exs
@@ -3,32 +3,27 @@ defmodule NetrcTest do
 
   test "read sample netrc" do
     netrc = Netrc.read("test/data/sample.netrc")
-    assert is_map(netrc)
     assert Map.get(netrc, "m") == %{ "login" => "l", "password" => "p" }
   end
 
   test "read sample netrc reversed" do
     netrc = Netrc.read("test/data/sample_reverse.netrc")
-    assert is_map(netrc)
     assert Map.get(netrc, "m") == %{ "login" => "l", "password" => "p" }
   end
 
   test "read sample netrc multi" do
     netrc = Netrc.read("test/data/multi.netrc")
-    assert is_map(netrc)
     assert Map.get(netrc, "m") == %{ "login" => "l", "password" => "p" }
     assert Map.get(netrc, "m2") == %{ "login" => "l2", "password" => "p2" }
   end
 
   test "default" do
     netrc = Netrc.read("test/data/default.netrc")
-    assert is_map(netrc)
     assert Map.get(netrc, "default") == %{ "login" => "l", "password" => "p" }
   end
 
   test "multi with default" do
     netrc = Netrc.read("test/data/multi_with_default.netrc")
-    assert is_map(netrc)
     assert Map.get(netrc, "m") == %{ "login" => "l", "password" => "p" }
     assert Map.get(netrc, "m2") == %{ "login" => "l2", "password" => "p2" }
     assert Map.get(netrc, "default") == %{ "login" => "dl", "password" => "dp" }
